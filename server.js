09e9f12e1a7d97abeeea09d0261771220ce06bfe
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2016-12-29 01:07:26 -0500

    Nodejs Http-server is replaced with express, with basic routing to home page

--- server.js
+++ server.js
@@ -1,50 +1,30 @@
 /*
-* HTTP server and client
-* The HTTP interfaces in Node.js are designed to support many features of the protocol
-* which have been traditionally difficult to use.
-* In order to support the full spectrum of possible HTTP applications,
-* Node.js's HTTP API is very low-level.
-* It deals with stream handling and message parsing only.
-* It parses a message into headers and body but it does not parse the actual headers or the body.
+* Express is a minimal and flexible Node.js web application framework
+* that provides a robust set of features for web and mobile applications
 * */
-var http = require("http");
+var express = require("express");
+
+var app = express();
 
 /*
- * Step 1: The server object returned here is an EventEmitter
- * */
-var server = http.createServer();
-
-function requestHandler (request,response){
+* Routing refers to the definition of application end points (URIs)
+* and how they respond to client request
+* */
+app.get('/', function (req, res) {
     /*
-    * Default Response Headers
-    * 1. connection: keep-alive
-    * 2. Date: xxxxxxx
-    * 3. Transfer-Encoding:chunked
-    *       When an HTTP client is reading a response message from a server it needs to know when it has reached the end of the message.'
-    *       This is particularly important with persistent (keep alive) connections,
-    *       because a connection can only be re-used by another HTTP transaction after the response message has been fully received.
-    *       There are four ways in which an HTTP server can indicate the end of the response message:
-    * */
-    response.write('<h1>Response from server for request</h1>');
-    response.write('<div>Default Headers</div>');
-    response.write('<ul><li>connection: keep-alive</li>');
-    response.write('<li>Date: xxxxxxx</li>');
-    response.write('<li>Transfer-Encoding:chunked</li></ul>');
-}
+     * Default Response Headers for Express
+     * 1. connection: keep-alive
+     * 2. Content-Length: xxx
+     * 3. Date:Thu, 29 Dec 2016 06:03:52 GMT
+     * 4. ETag:W/"2f-hkMaYB5nNxRiNce+hEnZQw"
+     * 5. content-type:text/html; charset=utf-8
+     * */
+    res.send('Express wrape nodejs and provides low level API');
+});
 
-// Step 2 : Event-Listener:requestHandler is added for Event: 'request<http.IncomingMessage>'
-/*
-* A persistent connection (HTTP persistent connection | connection: keep-alive) is a network communication channel
-* that remains open for further HTTP requests and responses rather than closing after a single exchange.
-* Persistent connections added for HTTP 1.0 used an extra header to request the client keep the connection alive;
-* HTTP 1.1 assumes all connections to be persistent unless otherwise specified.
-* HTTP 2.0 expands the persistent connection to enable numerous requests and returns of data to be exchanged
-* simultaneously over a single connection.
-* */
-server.on('request',requestHandler);
-
-// Step 3: to server for request Event  we need to call the listen method to initiate
-server.listen(8080);
+app.listen(8080, function () {
+    console.log('Express server listening on port %s', 8080);
+});
 
 
 
