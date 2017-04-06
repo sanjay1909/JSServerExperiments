9fd3067fbd064d177d368f7662950af18c42d7ad
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2017-04-06 13:31:04 -0400

    Cookie parser and Client session

--- server.js
+++ server.js
@@ -4,6 +4,10 @@
 * */
 var express = require("express");
 var path = require("path");
+var sessions = require("client-sessions");
+var cookieParser = require('cookie-parser');
+var index = require('./index');
+console.log(index);
 /*
 * Parse incoming request bodies in a middleware before our handlers,
 * Available under the req.body property.
@@ -11,6 +15,19 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
+
+app.use(sessions({
+    cookieName: 'sanjaySession', // cookie name dictates the key name added to the request object
+    secret: 'sanjay', // should be a large unguessable string
+    duration: 109600000, // how long the session will stay valid in ms
+    activeDuration: 109600000, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+    cookie: {
+        path: '/',
+        ephemeral: true, // when true, cookie expires when the browser closes
+        httpOnly: false, // when true, cookie is not accessible from javascript
+        secure: false // when true, cookie will only be sent over SSL. use key 'secureProxy' instead if you handle SSL not in your node process
+    }
+}));
 
 /*
 * To serve static files such as images, CSS files, and JavaScript files,
@@ -50,25 +67,36 @@ app.get('/RedirectedPage.html', function (req, res) {
     res.send("Static Resource served from ./ dir");
 });
 
-app.post('/index.html', function (req, res) {
+app.post('/launch',
+    function (req, res,next) {
+        // adding property to cookie name 'sanjaySession' is important
+        // else Set-Cookie wont be added as header res
+        // client-session library adds writeHead function to res
+        // which updates the cookie and add header only if there is a property in session
+        // After that all the request headers comes with cookie header
+        req.sanjaySession.launch = true;
+        next();
+    }, function (req, res) {
     console.log(req.body);
     if(req.body.type === 'Form post'){
         /*
         * redirect is GET request to that url
-        * You can't POST using a redirect.
+        * You can't POST using a redirect.s
         * By definition a redirect means the server sending a 302 redirect HTTP status code
         * to the client with the new location
         * so that the client issues a GET request to this new location
         * */
         res.redirect(302,'./RedirectedPage.html');
     } else {
-        res.redirect(302,'./AJAXRedirect.html');
+        res.redirect(302,'./AJAXRedirect.html?key=value');
     }
 
 });
 
-app.listen(8080, function () {
-    console.log('Express server listening on port %s', 8080);
+//app.set('port', process.env.PORT || 8078);
+
+app.listen(8078, function () {
+    console.log('Express server listening on port %s', 8078);
 });
 
 
